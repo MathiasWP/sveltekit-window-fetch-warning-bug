ee4c105356cca8964f3684e4cb3c279598bae6dd
Mathias Picker <mattis123@live.no>
2022-05-23 12:27:39 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+	export const load = async function ({ fetch }) {
+        fetch('/favicon.png')
+
+		return {};
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
